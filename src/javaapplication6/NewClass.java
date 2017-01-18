041e56e36180c129b9a94d64a2d214fa159bf72d
teslaaa <jai_c6@hotmail.com>
2017-01-18 23:09:33 +0000

    Update NewClass.java

--- src/javaapplication6/NewClass.java
+++ src/javaapplication6/NewClass.java
@@ -14,6 +14,6 @@ public class NewClass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("this i ra");
+        System.out.print("this i rankn,");
     }
 }

9c827d5259f61a862caddb069699e13ec3e57044
J <J@J-Laptop>
2017-01-18 22:37:09 +0000

    Merge origin/master
    Conflicts:
    	src/javaapplication6/NewClass.java

--- src/javaapplication6/NewClass.java
+++ src/javaapplication6/NewClass.java
@@ -14,6 +14,6 @@ public class NewClass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("hihi bywe");
+        System.out.print("hihi bywe test1");
     }
 }
